382090b0d276879febde8bd6d8f503de75df6f34
cewalike <cewalike@ucdavis.edu>
2024-11-14 15:00:41 -0800

    week 7 homework edits, week 8 inclass work

--- scripts/week_7_homework.R
+++ scripts/week_7_homework.R
@@ -7,7 +7,7 @@ library("RColorBrewer")
 
 gapminder <- read_csv("https://ucd-r-davis.github.io/R-DAVIS/data/gapminder.csv")
 
-#filter for years 2002 & 2007
+#filter for years 2002 & 2007 and continents of interest
 year_filter <- gapminder %>% 
   filter((year == 2002 | year == 2007) & continent != "Oceania") %>%
   pivot_wider(id_cols = c(continent, country), names_from = year, values_from = pop)
@@ -17,6 +17,7 @@ year_filter
 #calculate population difference
 year_diff <- year_filter %>%
   mutate(pop_diff = year_filter[4] - year_filter[3]) %>%
+  #could also use back ticks `2007` - `2002`
   arrange(pop_diff)
 
 #Then change the levels inside the country column to represent the order of the data
@@ -35,4 +36,5 @@ ggplot(year_diff, aes(x= country, y= pop_diff$'2007', fill = continent)) +
   facet_wrap(~ continent, scales = "free", as.table = F, axis.labels = "all_x") +
   labs(x = "Country", y = "Life Change in Population Between 2002 and 2007") +
   theme(axis.title = element_text(size = 12, color = "black")) +
-  guides(fill = "none", x = guide_axis(angle=45), axis.title = element_text(hjust = 1))
+  guides(fill = "none", x = guide_axis(angle=45), axis.title = element_text(hjust = 1)) +
+  coord_flip()
---
+++ scripts/week_8_function.R
@@ -0,0 +1,4 @@
+f_to_k <- function(tempF){
+  k <- ((tempF - 32) * (5/9)) + 273.15 
+  return(k)
+}
