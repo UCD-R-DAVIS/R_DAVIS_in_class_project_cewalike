343d2ad2755b961b7ebc0c213e71f70bacd9e2a9
cewalike <cewalike@ucdavis.edu>
2024-11-14 12:21:19 -0800

    week 7 homework complete

--- scripts/week_7_homework.R
+++ scripts/week_7_homework.R
@@ -2,6 +2,8 @@
 
 library(tidyverse)
 library(ggplot2)
+library("RColorBrewer")
+
 
 gapminder <- read_csv("https://ucd-r-davis.github.io/R-DAVIS/data/gapminder.csv")
 
@@ -17,16 +19,20 @@ year_diff <- year_filter %>%
   mutate(pop_diff = year_filter[4] - year_filter[3]) %>%
   arrange(pop_diff)
 
+#Then change the levels inside the country column to represent the order of the data
+#from Googling
+
+year_diff$country <- as.vector(year_diff$country) #get rid of factors
+year_diff$country = factor(year_diff$country,year_diff$country) #add ordered factors back
 year_diff
 
+display.brewer.all(colorblindFriendly = TRUE)
 
 #plot panel
 ggplot(year_diff, aes(x= country, y= pop_diff$'2007', fill = continent)) +
   geom_col() +
+  scale_fill_brewer(palette = "Dark2") +
   facet_wrap(~ continent, scales = "free", as.table = F, axis.labels = "all_x") +
   labs(x = "Country", y = "Life Change in Population Between 2002 and 2007") +
   theme(axis.title = element_text(size = 12, color = "black")) +
-  guides(fill = "none")
-
-?facet_wrap  
-
+  guides(fill = "none", x = guide_axis(angle=45), axis.title = element_text(hjust = 1))
