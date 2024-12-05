131cb662324b6c89950d28cc4b33cf274f20b7f4
cewalike <cewalike@ucdavis.edu>
2024-12-05 14:04:09 -0800

    week 9 homework edits

--- scripts/week_9_homework.R
+++ scripts/week_9_homework.R
@@ -1,4 +1,5 @@
 # Week 9 homework
+setwd('C:/Fall-2024/R-Projects/R_DAVIS_in_class_project_cewalike/')
 library(tidyverse)
 
 # Load surveys data
@@ -9,9 +10,9 @@ surveys <- surveys %>%
   mutate(speciesLength = nchar(species))
 
 for(i in unique(surveys$taxa)) {
-  eachTaxon <- surveys[surveys$taxa == i,]
+  eachTaxon <- surveys[surveys$taxa == i,] # Filter for the unique taxa only stored in i
   speciesIndex <- which(eachTaxon$speciesLength == max(eachTaxon$speciesLength))
-  print(unique(eachTaxon$taxa))
+  print(i)
   print(unique(eachTaxon$species[speciesIndex]))
 }
 
@@ -24,7 +25,7 @@ mloa <- read_csv("https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/master/data
 mloa %>% 
   select(windDir,windSpeed_m_s,baro_hPa,temp_C_2m,temp_C_10m,
           temp_C_towertop,rel_humid, precip_intens_mm_hr) %>%
-  map(max) # gives a list
+  map(max, na.rm = T) # gives a list
 
 # Make a function called C_to_F that converts Celsius to Fahrenheit. 
 # Hint: first you need to multiply the Celsius temperature by 1.8, then add 32
@@ -37,8 +38,19 @@ C_to_F <- function(tempC){
 # by applying this function to columns “temp_C_2m”, “temp_C_10m”, and “temp_C_towertop”.
 mloaF <- mloa %>%
   select(temp_C_2m, temp_C_10m, temp_C_towertop) %>%
-  map_dfc(C_to_F)
+  map_dfc(C_to_F) # %>% rename("temp_F_2m" = "temp_C_2m"...)
 
 new_names <- c("temp_F_2m", "temp_F_10m", "temp_F_towertop")
-colnames(mloaF) <- new_names
+colnames(mloaF) <- new_names # Could also use rename function
+
+# Challenge: use lapply to make a new column that includes genus and species together
+# as a string
+surveys %>% mutate(genusspecies = lapply(1:nrow(surveys), function(i){
+  paste0(surveys$genus[i], " ", surveys$species[i])
+}))
+
+install.packages("stringr")
+library("stringr")
+
+str_replace("temp_F_2m", "F", "C")
     
